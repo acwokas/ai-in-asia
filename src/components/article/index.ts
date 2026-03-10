53cc692efb3004d6b391e1876168dab17a473d8c
acwokas <me@adrianwatkins.com>
2026-03-11 00:27:57 +0800

    Remove ArticleMetaHead export from barrel file

--- src/components/article/index.ts
+++ src/components/article/index.ts
@@ -3,7 +3,6 @@ export { useArticleShareHandlers } from './ArticleShareHandlers';
 export { renderArticleContent } from './ArticleContentRenderer';
 export { default as ArticleHeader } from './ArticleHeader';
 export { ArticleAuthorFooter, ArticleAuthorBio } from './ArticleAuthorBio';
-export { ArticleMetaHead } from './ArticleMetaHead';
 export { ArticleSponsorBanner } from './ArticleSponsorBanner';
 export { ArticleLoadingSkeleton } from './ArticleLoadingSkeleton';
 export { ArticleNotFound } from './ArticleNotFound';
